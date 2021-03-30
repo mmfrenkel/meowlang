d482e61bb76e40ae381fc6acd0b0fbd202e22f24
Lauren Pham <lauren.pham@focus.org>
2021-03-30 18:20:40 -0400

    interim

--- src/import.ml
+++ src/import.ml
@@ -1,5 +1,8 @@
 (*
-  Performs import-specific semantic checks on the AST, producing a new SAST
+  Performs import-specific semantic checks on the AST,
+  Scans and parses each import file,
+  Produces AST which is the importing file's AST appended by
+  the ASTs of the import files appended
 *)
 open Exceptions
 open Ast
@@ -27,26 +30,36 @@ let check_import_names imports =
       List.iter check_name imports
 
 (* Scan each import file, returns a list of lexbuf *)
+(* imports: list of filenames *)
 let scan_imports imports =
+    (* change this to open, scan, close in one List.map step *)
     let channel_in fname = open_in fname in
       let scan channel = Lexing.from_channel channel in
         List.map scan (List.map channel_in imports)
         
 (* Parse each scenned import, returns a list of AST programs *)
+(* lexbuf_imports: list of Lexing.lexbuf *)
 let generate_asts lexbuf_imports =
   let parse_ast lexbuf = Parser.program Scanner.token lexbuf in
     List.map parse_ast lexbuf_imports
 
 (* Add functions from imports to the list of functions *)
-let add_import_functions existing_funcs imports =
-  let add_import_funcs existing_funcs import = List.map (fun func_name -> func_name :: existing_funcs ) import in
-    List.map add_import_funcs imports
+(* existing_funcs: list of funcs *)
+(* import_asts: import list * func_decl list * class_decl list *)
+let add_import_functions existing_funcs import_asts =
+  let add_import_funcs existing_funcs import = List.map (fun (imports, func_decl, class_decl) -> func_decl :: existing_funcs ) import in
+    List.map add_import_funcs import_asts
 
 (* Add classes from imports to the list of classes *)
-let add_import_classes existing_classes imports =
-  let add_import_classes existing_classes import = List.map (fun func_name -> func_name :: existing_classes ) import in
-    List.map add_import_classes imports
+(* existing_classes: list of classes *)
+(* import_asts: import list * func_decl list * class_decl list *)
+let add_import_classes existing_classes import_asts =
+  let add_import_classes existing_classes import = List.map (fun (imports, func_decl, class_decl) -> class_decl :: existing_classes ) import in
+    List.map add_import_classes import_asts
 
+(* imports: list of filename imports from importing program *)
+(* functions: list of func_decls *)
+(* classes: list of class_decls *)
 let add (imports, functions, classes) =
   (* check that import file in expected location *)
   find_imports imports;
@@ -61,5 +74,7 @@ let add (imports, functions, classes) =
       let functions' = add_import_functions functions import_asts in
       (* add classes from each import_ast to classes *)
       let classes' = add_import_classes classes import_asts in 
+
+      (* something like pattern matching to check if imports have imports? *)
   
   (functions', classes')
