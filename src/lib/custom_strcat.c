2f570e761161b2060b2650a76583367f8b3f2e46
Lauren Pham <lauren.pham@focus.org>
2021-04-17 12:32:36 -0400

    custom_strcat segfault

---
+++ src/lib/custom_strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+char *custom_strcat(char *lhs, char *rhs)
+{
+  printf("%s", lhs);
+  printf("%s", rhs);
+
+  /* the size of the concatenated string */
+  int size_of_buffer = strlen(lhs) + strlen(rhs) + 1;
+
+  /* malloc space for the new string */
+	char *buffer;
+	buffer = malloc(sizeof(char) * size_of_buffer);
+	if (!buffer) {
+		printf("%s\n", strerror(errno));
+		return "\0";
+	}
+
+  memcpy(buffer, lhs, strlen(lhs));
+  memcpy(buffer+strlen(rhs), rhs, strlen(rhs)+1);
+  /* null terminate the buffer */
+	buffer[strlen(buffer) + 1]= '\0';
+  printf("%s", buffer);
+  return '\0'; // buffer gets printed correctly at this point
+  return buffer; // segfault at this point
+}
